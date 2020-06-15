907e67721262bcd0721917084fa67fbe09acbf4c
david.scheers@gmail.com <MW^ol9sAdWxNgFK*f5Z36IK*J>
2020-06-15 22:19:49 +0200

    add tests for basic scenario

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,41 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void normalItem_SellInAndQualityDecreaseByOne() {
+        Item[] items = new Item[]{new Item("foo", 10, 10)};
+
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        List<Item> expected = Collections.singletonList(new Item("foo", 9, 9));
+        assertEquals(expected, Arrays.asList(app.items));
     }
 
+    @Test
+    void normalItem_SystemCanProcessMultipleItems() {
+        Item[] items = new Item[] {new Item("foo", 10, 10),
+                new Item("Elixir of the Mongoose", 5, 7)
+        };
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+
+        Item[] expectedArray = new Item[] {new Item("foo", 9, 9),
+                new Item("Elixir of the Mongoose", 4, 6)
+        };
+        List<Item> expected = Arrays.asList(expectedArray);
+
+        assertEquals(expected, Arrays.asList(app.items));
+
+    }
 }
